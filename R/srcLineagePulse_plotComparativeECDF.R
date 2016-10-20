1662d9f6d48eec05712954536e3b923f0360e341
davidsebfischer <david.seb.fischer@gmail.com>
2016-10-20 16:53:01 +0200

    Improved comparative ECDF plotting.

--- R/srcLineagePulse_plotComparativeECDF.R
+++ R/srcLineagePulse_plotComparativeECDF.R
@@ -26,27 +26,35 @@ plotComparativeECDF <- function(
   strRunLabels,
   strTitle="Comparative Q-value ECDF"){
   
+  if(length(lsDirOutLineagePulse) != length){
+    stop("Number of supplied directories and run names dont match.")
+  }
   lsVecECDF <- list()
+  vecNAnalysed <- array(NA, length(lsDirOutLineagePulse))
   vecX <- seq(-100,-1,by=0.1)
   for(dirLP in lsDirOutLineagePulse){
     load(paste0(dirLP,"/LineagePulse_dfDEAnalysis.RData"))
     lsVecECDF[length(lsVecECDF)+1] <- list(sapply(vecX, function(thres){
       sum(log(as.numeric(as.vector(dfDEAnalysis$adj.p)))/log(10) <= thres, na.rm=TRUE)}))
+    vecNAnalysed[length(lsVecECDF)] <- sum(!is.na(as.numeric(as.vector(dfDEAnalysis$adj.p))))
   }
   
   # Generate data frame for ggplot
   dfECDF <- data.frame(
     logq=rep(vecX, length(lsVecECDF)),
     counts=unlist(lsVecECDF),
-    run=unlist(lapply(strRunLabels, function(run) rep(run, length(vecX)))) )
+    data_set=unlist(lapply( seq(1, length(strRunLabels)), function(i){
+      rep( paste(strRunLabels[i], "[", vecNAnalysed[i],"]"),  
+        length(vecX))
+      })) )
   # ggplot
-  gECDF <- ggplot(dfECDF, aes(x=logq, y=counts, group=run)) +
-    geom_line(aes(colour=run)) +
+  gECDF <- ggplot(dfECDF, aes(x=logq, y=counts, group=data_set)) +
+    geom_line(aes(colour=data_set)) +
     labs(title=strTitle) +
     xlab(paste0("log_10(q-value)")) +
     ylab(paste0("Cumulative q-value distribution")) +
     xlim(-15,-1) + 
-    ylim(0,max(unlist(lsVecECDF))) +
+    ylim(0,max(unlist(lsVecECDF))) + 
     theme(axis.text=element_text(size=14),
       axis.title=element_text(size=14,face="bold"),
       title=element_text(size=14,face="bold"),
