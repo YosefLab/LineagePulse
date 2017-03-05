12d52d16b1ef9e64e550f0d7b70a094339dc89e0
David Fischer <david@Davids-MBP.fritz.box>
2017-03-05 23:34:49 +0100

    Removed prin vecLL from fitZINB

--- R/srcLineagePulse_fitZINB.R
+++ R/srcLineagePulse_fitZINB.R
@@ -261,12 +261,10 @@ fitZINB <- function(matCounts,
 			                     "reference ll of  ", sum(vecLogLikRef), " in ",
 			                     round(tm_mudisp["elapsed"]/60,2)," min.")
 			print(strMessage)
-			print(vecLogLikRef)
 			
 			# Evaluate Likelihood
 			scaLogLikOld <- scaLogLikNew
 			scaLogLikNew <- sum(lsFitMuDisp$vecLL)
-			print(lsFitMuDisp$vecLL)
 		})
 		
 		# Iteration complete
